1bbcc204a38aa584db1a4b4f3a3b5c0fbec1dfb5
roryf <rory,ferguson@hotmail.com>
2022-05-31 23:07:30 +0100

    Post mappings for AppUser and Roles that returns a 201 http status code

--- src/main/java/com/rory/userservice/api/AppUserController.java
+++ src/main/java/com/rory/userservice/api/AppUserController.java
@@ -1,13 +1,14 @@
 package com.rory.userservice.api;
 
 import com.rory.userservice.domain.AppUser;
+import com.rory.userservice.domain.Role;
 import com.rory.userservice.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,18 @@ public class AppUserController {
     @GetMapping("/users")
     public ResponseEntity<List<AppUser>>getAppUsers(){
         return ResponseEntity.ok().body(userService.getAppUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser> saveAppUser(@RequestBody AppUser user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.ok().body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
     }
 
 }
